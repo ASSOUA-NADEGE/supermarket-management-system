e6f1568bf9e5f90d8c9c9327be5d8946397b691f
John-The-Boss <johnthbss@gmail.com>
2024-03-21 09:56:03 +0100

    added field for sku and stock on product migration

--- database/migrations/2024_03_19_040809_create_products_table.php
+++ database/migrations/2024_03_19_040809_create_products_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('sku')->unique();
+            $table->integer('stock')->default(0)->after('sku');
+            $table->string('image');
             $table->integer('price');
             $table->string('description');
             $table->integer('quantity');
