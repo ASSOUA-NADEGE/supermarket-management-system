89aa0a708561b8fd65fd5a0b7ee2e6500e75bae0
John-The-Boss <johnthbss@gmail.com>
2024-03-21 22:44:09 +0100

    welcomecontroller created

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function landing()
+    {
+        return inertia('Welcome',[
+            "categories" => Category::all(),
+            "popular_products" => Product::all()->take(8),
+        ]);
+    }
+}
