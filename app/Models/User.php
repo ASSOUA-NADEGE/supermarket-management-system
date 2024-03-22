5a72c8a7f4be8d22a6f6a47a223086981a5f4a48
Minister <mundestephane13@gmail.com>
2024-03-22 15:17:55 +0100

    Addded a user observer and attached it to the user model
    This observer will help to hash the password when creating a new user...
    It will do this if ever the password has not been hashed

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
+#[ObservedBy(\App\Observers\UserObserver::class)]
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRoles;
