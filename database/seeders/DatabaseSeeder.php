aff86de86e0b51ef2ea4989197699acf1b09c02c
Minister <mundestephane13@gmail.com>
2024-03-22 15:29:35 +0100

    Made the database seeder class more declarative

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\User;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 
 class DatabaseSeeder extends Seeder
@@ -19,35 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-
         $this->call([
             PermissionSeeder::class,
             RoleSeeder::class,
+            UserSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class
         ]);
-
-        $admin = Role::where('name', 'admin')->first();
-        $vendor = Role::where('name', 'vendor')->first();
-
-        $minister = User::factory()->create([
-            "name" => "minister",
-            "password" => bcrypt('minister')
-        ]);
-        $johntheboss = User::factory()->create([
-            "name" => "johntheboss",
-            "password" => bcrypt('johntheboss')
-        ]);
-
-        $nadege = User::factory()->create([
-            "name" => "nadege",
-            "password" => bcrypt('nadege')
-        ]);
-
-        $minister->assignRole($admin);
-        $johntheboss->assignRole($admin);
-        $nadege->assignRole($vendor);
-
     }
 }
