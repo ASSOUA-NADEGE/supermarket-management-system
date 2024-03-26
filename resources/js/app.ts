ab4eb800745c838b414f8f7c5283e989db227745
Minister <mundestephane13@gmail.com>
2024-03-26 13:12:58 +0100

    Added pinia to the vue app

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,31 +1,37 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import PrimeVue from 'primevue/config';
-import Ripple from 'primevue/ripple';
-import StyleClass from 'primevue/styleclass';
+import { createApp, h, DefineComponent } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy";
+import PrimeVue from "primevue/config";
+import Ripple from "primevue/ripple";
+import StyleClass from "primevue/styleclass";
+import { createPinia } from "pinia";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue"),
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(PrimeVue,{
+            .use(PrimeVue, {
                 ripple: true,
             })
-            .directive('ripple', Ripple)
-            .directive('styleclass', StyleClass)
+            .directive("ripple", Ripple)
+            .directive("styleclass", StyleClass)
             .use(ZiggyVue)
+            .use(createPinia())
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
