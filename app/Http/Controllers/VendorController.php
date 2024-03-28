b5c901a076cb60f7a37220e78f3b6820d635b635
Minister <mundestephane13@gmail.com>
2024-03-28 23:33:48 +0100

    Added authorization constraints to VendorController

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,28 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class VendorController extends Controller
 {
-    public function dashboard(){
+    public function dashboard()
+    {
         return inertia('vendor/Dashboard');
     }
 
-    public function createOrder(){
-        return inertia('vendor/order/Create',['products'=>Product::all(),'categories'=>Category::all()]);
+    public function createOrder()
+    {
+        abort_unless(request()->user()->cannot('create', Order::class), Response::HTTP_FORBIDDEN);
+
+        return inertia('vendor/order/Create', ['products' => Product::all(), 'categories' => Category::all()]);
     }
 
-    public function indexOrder(){
+    public function indexOrder()
+    {
         return inertia('vendor/order/Index');
     }
 
-    public function indexProduct(){
+    public function indexProduct()
+    {
+        abort_unless(request()->user()->can('viewAny', Product::class), Response::HTTP_FORBIDDEN);
+
         return inertia('vendor/product/Index');
     }
 
-    public function profile(){
+    public function profile()
+    {
         return inertia('vendor/profile/Edit');
     }
 }
