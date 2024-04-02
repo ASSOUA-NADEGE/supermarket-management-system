4980772495578740b9cd95084ee89a5fe0ea59f6
Minister <mundestephane13@gmail.com>
2024-04-02 16:01:29 +0100

    Added statistics for daily, weekly and monthly sales

--- resources/js/Pages/vendor/Dashboard.vue
+++ resources/js/Pages/vendor/Dashboard.vue
@@ -1,39 +1,80 @@
 <script setup lang="ts">
+import { reactive } from "vue";
+import {
+    differenceInDays,
+    differenceInWeeks,
+    differenceInMonths,
+} from "date-fns";
 import VendorLayout from "@/Layouts/VendorLayout.vue";
-import Card from "primevue/card"
-import Button from "primevue/button"
+import Card from "primevue/card";
+import Button from "primevue/button";
 import SalesChart from "@/Components/SalesChart.vue";
-import {Link} from "@inertiajs/vue3"
+import { Link } from "@inertiajs/vue3";
 import ProductSalesChart from "@/Components/ProductSalesChart.vue";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import DefaultLayout from "@/Layouts/DefaultLayout.vue";
 
 defineOptions({
-    layout: [DefaultLayout,VendorLayout]
-})
+    layout: [DefaultLayout, VendorLayout],
+});
+const props = defineProps<{ orders: Array<Record<string, any>> }>();
+
+const cards = reactive([
+    {
+        title: "Today's Orders",
+        quantity: props.orders.filter(
+            (order) => differenceInDays(new Date(), order.created_at) <= 1,
+        ).length,
+        percentage: "+3%",
+    },
+    {
+        title: "This week's Orders",
+        quantity: props.orders.filter(
+            (order) => differenceInWeeks(new Date(), order.created_at) <= 1,
+        ).length,
+        percentage: "+7%",
+    },
+    {
+        title: "This month's Orders",
+        quantity: props.orders.filter(
+            (order) => differenceInMonths(new Date(), order.created_at) <= 1,
+        ).length,
+        percentage: "+7%",
+    },
+]);
 </script>
 
 <template>
     <div class="space-y-4 px-4 py-2">
         <Link href="/vendor/orders/create">
-            <Button label="New Order" icon="pi pi-plus" icon-pos="right"></Button>
+            <Button
+                label="New Order"
+                icon="pi pi-plus"
+                icon-pos="right"
+            ></Button>
         </Link>
         <div class="grid grid-cols-3 gap-8">
-            <Card v-for="i in 3" class="rounded-none">
+            <Card v-for="card in cards" class="rounded-none">
                 <template #content>
                     <div class="space-y-0">
-                        <h5>Today's Order</h5>
-                        <h1>2500</h1>
+                        <h5>{{ card.title }}</h5>
+                        <h1>{{ card.quantity }}</h1>
                         <div class="flex items-center justify-between">
-                            <p class="text-sm"><span>+31%</span>than last week</p>
-                            <Button outlined aria-readonly="true" icon="pi pi-arrow-up"></Button>
+                            <p class="text-sm">
+                                <span>{{ card.percentage }}</span>
+                            </p>
+                            <Button
+                                outlined
+                                aria-readonly="true"
+                                icon="pi pi-arrow-up"
+                            ></Button>
                         </div>
                     </div>
                 </template>
             </Card>
-            <SalesChart class="col-span-2"/>
-            <ProductSalesChart/>
+            <SalesChart class="col-span-2" />
+            <ProductSalesChart />
         </div>
         <Card class="rounded-none">
             <template #content>
@@ -48,6 +89,4 @@ defineOptions({
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
