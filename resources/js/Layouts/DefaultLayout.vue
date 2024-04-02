adc6b4583913d8b63dbce8853c76e14af027ad83
Minister <mundestephane13@gmail.com>
2024-04-02 20:42:55 +0100

    Added a timeout delay to loader screen
    This delay was added to improve user experience...

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
-import Loader from "@/Components/Loader.vue"
-import {router} from '@inertiajs/vue3'
-import {ref, onMounted} from "vue";
+import Loader from "@/Components/Loader.vue";
+import { router } from "@inertiajs/vue3";
+import { ref, onMounted } from "vue";
+import { debounce } from "lodash";
+import { useTimeoutFn } from "@vueuse/core";
 
-const routing = ref(false)
+const routing = ref(false);
 
 onMounted(() => {
-    router.on('start', () => {
-        routing.value = true
-    })
-    router.on('finish', () => {
-        routing.value = false
-    })
-})
+    router.on("start", () => {
+        routing.value = true;
+    });
+    router.on("finish", () => {
+        useTimeoutFn(() => {
+            routing.value = false;
+        }, 2000);
+    });
+});
 </script>
 
 <template>
-    <Loader v-if="routing"/>
+    <Loader v-if="routing" />
     <div v-else>
-        <slot/>
+        <slot />
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
