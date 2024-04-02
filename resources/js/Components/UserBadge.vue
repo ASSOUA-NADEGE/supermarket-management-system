b93904a921b94130cebd8af4a7e3028e56a9fcbd
Minister <mundestephane13@gmail.com>
2024-04-02 20:26:59 +0100

    Added logout functionality to the frontend

--- resources/js/Components/UserBadge.vue
+++ resources/js/Components/UserBadge.vue
@@ -1,32 +1,30 @@
 <script setup lang="ts">
 import OverlayPanel from "primevue/overlaypanel";
-import ConfirmDialog from 'primevue/confirmdialog';
-import {useConfirm} from "primevue/useconfirm";
-import {ref} from "vue";
+import ConfirmDialog from "primevue/confirmdialog";
+import { useConfirm } from "primevue/useconfirm";
+import { ref } from "vue";
 import Avatar from "primevue/avatar";
-import {Link} from "@inertiajs/vue3";
+import { Link, router } from "@inertiajs/vue3";
 
 const confirm = useConfirm();
 
 const op = ref();
 const toggle = (event: any) => {
     op.value.toggle(event);
-}
+};
 const confirm1 = () => {
     confirm.require({
-        message: 'Are you sure you want to logout?',
-        header: 'Danger Zone',
-        icon: 'pi pi-info-circle',
-        rejectLabel: 'Cancel',
-        acceptLabel: 'Logout',
-        rejectClass: 'p-button-secondary p-button-outlined',
-        acceptClass: 'p-button-danger',
+        message: "Are you sure you want to logout?",
+        header: "Danger Zone",
+        icon: "pi pi-info-circle",
+        rejectLabel: "Cancel",
+        acceptLabel: "Logout",
+        rejectClass: "p-button-secondary p-button-outlined",
+        acceptClass: "p-button-danger",
         accept: () => {
-            alert('done');
+            router.post("/logout");
         },
-        reject: () => {
-            alert('failed');
-        }
+        reject: () => {},
     });
 };
 </script>
@@ -35,18 +33,25 @@ const confirm1 = () => {
     <div>
         <div class="flex items-center gap-2 cursor-pointer" @click="toggle">
             <p class="capitalize">{{ $page.props.auth.user.name }}</p>
-            <Avatar :label="$page.props.auth.user.name[0].toUpperCase()" shape='circle'/>
+            <Avatar
+                :label="$page.props.auth.user.name[0].toUpperCase()"
+                shape="circle"
+            />
         </div>
         <ConfirmDialog></ConfirmDialog>
         <OverlayPanel ref="op" class="overlow-hidden">
             <template #container>
-                <Link href="/profile"
-                      class="flex items-center gap-3 px-4 py-2 text-neutral-600 hover:bg-neutral-400/10 text-sm">
+                <Link
+                    href="/profile"
+                    class="flex items-center gap-3 px-4 py-2 text-neutral-600 hover:bg-neutral-400/10 text-sm"
+                >
                     <i :class="`pi pi-user`"></i>
                     <span>Profile</span>
                 </Link>
-                <button @click="confirm1()"
-                        class="flex items-center gap-3 px-4 py-2 text-neutral-600 hover:bg-neutral-400/10 text-sm">
+                <button
+                    @click="confirm1()"
+                    class="flex items-center gap-3 px-4 py-2 text-neutral-600 hover:bg-neutral-400/10 text-sm"
+                >
                     <i :class="`pi pi-sign-out`"></i>
                     <span>Logout</span>
                 </button>
@@ -55,6 +60,4 @@ const confirm1 = () => {
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
