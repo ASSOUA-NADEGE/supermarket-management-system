732ee65c34ce1d046b0ffb7f320f17cc6b2098df
Minister <mundestephane13@gmail.com>
2024-04-02 20:17:15 +0100

    Configured middlewares redirect for auth & guest

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectUsersTo(fn () => request()->user()->hasRole('admin') ? '/admin' : '/vendor');
+        $middleware->redirectGuestsTo('/auth');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
