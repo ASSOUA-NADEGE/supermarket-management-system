493cf6ea7ecd97282e8f8f72f6aef2e89ce3c776
Minister <mundestephane13@gmail.com>
2024-04-02 20:28:43 +0100

    Protected the admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -4,13 +4,13 @@
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->group(function(){
-    Route::redirect('/','admin/dashboard');
-    Route::get('dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
-    Route::get('users',[AdminController::class,'indexUsers'])->name('admin.users');
-    Route::get('users/create',[AdminController::class,'createUsers'])->name('admin.users.create');
-    Route::get('products',[AdminController::class,'indexProducts'])->name('admin.products');
-    Route::get('products/create',[AdminController::class,'createProducts'])->name('admin.products.create');
-    Route::get('orders',[AdminController::class,'indexOrders'])->name('admin.orders');
-    Route::get('categories',[AdminController::class,'indexCategories'])->name('admin.categories');
+Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
+    Route::redirect('/', 'admin/dashboard');
+    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('users', [AdminController::class, 'indexUsers'])->name('admin.users');
+    Route::get('users/create', [AdminController::class, 'createUsers'])->name('admin.users.create');
+    Route::get('products', [AdminController::class, 'indexProducts'])->name('admin.products');
+    Route::get('products/create', [AdminController::class, 'createProducts'])->name('admin.products.create');
+    Route::get('orders', [AdminController::class, 'indexOrders'])->name('admin.orders');
+    Route::get('categories', [AdminController::class, 'indexCategories'])->name('admin.categories');
 });
