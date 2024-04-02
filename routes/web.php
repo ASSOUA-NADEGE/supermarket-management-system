6b952071c307d0648fd74938ef78a7efdbc0a08f
Minister <mundestephane13@gmail.com>
2024-04-02 20:18:17 +0100

    commented unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [WelcomeController::class, 'landing'])->name('landing');
+// Route::get('/', [WelcomeController::class, 'landing'])->name('landing');
 //Route::get('/products', [ProductController::class, 'index'])->name('products');
 Route::resource('products', ProductController::class);
 
@@ -28,32 +28,32 @@ Route::prefix('vendor')
 
 require base_path('routes/admin.php');
 
-Route::prefix('breeze')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
-    });
-
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-    require __DIR__ . '/auth.php';
-});
+// Route::prefix('breeze')->group(function () {
+//     Route::get('/', function () {
+//         return Inertia::render('Welcome', [
+//             'canLogin' => Route::has('login'),
+//             'canRegister' => Route::has('register'),
+//             'laravelVersion' => Application::VERSION,
+//             'phpVersion' => PHP_VERSION,
+//         ]);
+//     });
+//
+//     Route::get('/dashboard', function () {
+//         return Inertia::render('Dashboard');
+//     })->middleware(['auth', 'verified'])->name('dashboard');
+//
+//     Route::middleware('auth')->group(function () {
+//         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//     });
+//     require __DIR__ . '/auth.php';
+// });
 
 Route::prefix('auth')->middleware('guest')->group(function () {
     Route::controller(App\Http\Controllers\AuthenticationController::class)->group(function () {
         Route::post('', 'login');
-        Route::get('/', 'home')->name('login');
+        Route::get('/', 'home');
         Route::get('/{role}', 'role');
     });
 });
