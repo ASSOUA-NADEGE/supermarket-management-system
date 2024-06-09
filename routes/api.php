e8e7464035de8e7b5ce070994280f063659a7457
Minister <mundestephane13@gmail.com>
2024-06-09 01:40:31 +0100

    Implement search by category when creating order

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,31 @@
 <?php
 
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Pipeline;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/products', fn () => Product::query()->limit(10)->whereAny(['name', 'description', 'sku'], 'LIKE', '%' . request('q') . '%')->get());
+Route::get('/products', fn () =>
+    Pipeline::send(Product::query()->limit(10))->through([
+        // Fetch products matching the searched query
+        function (Builder $query, Closure $next) {
+            if (!empty(request('q')))
+                $query->whereAny(['name', 'description', 'sku'], 'LIKE', '%' . request('q') . '%');
+
+            return $next($query);
+        },
+
+        // Fetch products matching the category name
+        function (Builder $query, Closure $next) {
+            if (!empty(request('category')))
+                $query->whereRelation('category', 'name', request('category'));
+
+            return $next($query);
+        }
+    ])->then(fn (Builder $query) => $query->get())
+);
